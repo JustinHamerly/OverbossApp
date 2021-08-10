8ab9f8c1a629497e47e5fb7bba8c945607b5980b
Justin Hamerly <suvimountain@icloud.com>
2021-08-10 11:44:32 -0700

    shuffle algorithm for arrays and placeholder images

--- JS/app.js
+++ JS/app.js
@@ -7,11 +7,11 @@ const tilePool = [];
 const tokenPool = [];
 
 const terrainArray = ['Forest', 'Cave', 'Graveyard', 'Swamp', 'Camp', 'Castle', 'Cloud Island', 'Desert', 'Summoning Circle', 'Volcano', 'Tower'];
-const tokenArray = ['Kobold', 'Dragon', 'Skeleton', 'Witch', 'Orc', 'Vampire', 'Harpy', 'Sandoworm', 'Sorcerobe', 'Elemental', 'Evil Eye'];
+const tokenArray = ['Kobold', 'Dragon', 'Skeleton', 'Witch', 'Orc', 'Vampire', 'Harpy', 'Sandworm', 'Sorcerobe', 'Elemental', 'Evil Eye'];
 const crystalArray = ['Forest Crytal', 'Cave Crystal', 'Graveyard Crystal', 'Swamp Crystal', 'Camp Crystal', 'Castle Crystal', 'Cloud Island Crystal', 'Desert Crystal', 'Summoning Circle Crystal', 'Volcano Crystal', 'Tower Crystal'];
-const terrainImgArray = ['img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png'];
-const tokenImgArray = ['img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png'];
-const crystalImgArray = ['img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png', 'img.png'];
+const terrainImgArray = ['./img/Tiles/ForestTile.png', './img/Tiles/CaveTile.png', './img/Tiles/GraveyardTile.png', './img/Tiles/SwampTile.png', './img/Tiles/CampTile.png', './img/Tiles/CastleTile.png', './img/Tiles/CloudIslandTile.png', './img/Tiles/DesertTile.png', './img/Tiles/SummoningCircleTile.png', './img/Tiles/VolcanoTile.png', './img/Tiles/TowerTile.png'];
+const tokenImgArray = ['./img/Tokens/ForestToken.png', './img/Tokens/CaveToken.png', './img/Tokens/GraveyardToken.png', './img/Tokens/SwampToken.png', './img/Tokens/CampToken.png', './img/Tokens/CastleToken.png', './img/Tokens/CloudIslandToken.png', './img/Tokens/DesertToken.png', './img/Tokens/SummoningCircleToken.png', './img/Tokens/VolcanoToken.png', './img/Tokens/TowerToken.png'];
+const crystalImgArray = ['./img/Crystals/ForestCrystal.png', './img/Crystals/CaveCrystal.png', './img/Crystals/GraveyardCrystal.png', './img/Crystals/SwampCrystal.png', './img/Crystals/CampCrystal.png', './img/Crystals/CastleCrystal.png', './img/Crystals/CloudIslandCrystal.png', './img/Crystals/DesertCrystal.png', './img/Crystals/SummoningCircleCrystal.png', './img/Crystals/VolcanoCrystal.png', './img/Crystals/TowerCrystal.png'];
 
 
 // ------------------------------------------------------CONSTRUCTOR FUNCTIONS
@@ -55,7 +55,7 @@ function createForm(){
     let labelElem = newElement('label', formElem, terrainArray[i]);
     labelElem.for = terrainArray[i];
   }
-  let buttonElem = newElement('button', formElem, 'submit');
+  let buttonElem = newElement('button', formElem, 'SHUFFLE TILES');
   buttonElem.id = 'submitButton';
   buttonElem.disabled = true;
 }
@@ -86,13 +86,23 @@ function pickRandomNumber(array){
 }
 //grabs a random index number for the inputted array
 
+function shuffle(array) {
+  let m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+//Fisher Yates array shuffle algorithm.
 
 
 
 // ------------------------------------------------------EVENT FUNCTIONS
 function handleSubmit(e){
   e.preventDefault();
-  console.log(e);
   for (let i=0; i<terrainArray.length; i++){
     if (e.target[i].checked){
       for (let j=0; j<12; j++){
@@ -105,21 +115,20 @@ function handleSubmit(e){
     }
   }
   for (let i=0; i<8; i++){
-    tilePool.push(new Tile('Dungeon', 'dungeon.png'));
+    tilePool.push(new Tile('Dungeon', './img/Tiles/DungeonTile.png'));
   }
   for (let i=0; i<7; i++){
-    tokenPool.push(new Token('Portal', 'portal.png'));
+    tokenPool.push(new Token('Portal', './img/Tokens/PortalToken.png'));
   }
   for (let i=0; i<6; i++){
-    tokenPool.push(new Token('Miniboss', 'miniboss.png'));
+    tokenPool.push(new Token('Miniboss', './img/Tokens/MinibossToken.png'));
   }
-  console.log(tilePool);
-  console.log(tokenPool);
+  shuffle(tilePool);
+  shuffle(tokenPool);
 }
 //pushes tiles to the array by checking which boxes have been checked.  Also pushes all the standard tokens.
 
 function handleRandom(e){
-  console.log(e);
   let checkBoxes = document.forms.terrainSelect.terrainTypes;
   for (let i=0; i<terrainArray.length; i++){
     checkBoxes[i].checked = false;
@@ -151,3 +160,4 @@ randomButtonElem.addEventListener('click', handleRandom);
 // ------------------------------------------------------CALL FUNCTIONS
 createForm();
 checkboxLimit(document.forms.terrainSelect.terrainTypes, 5);
+
